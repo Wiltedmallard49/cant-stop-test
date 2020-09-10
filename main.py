8534925f14bb602526781b7d967e092ce6a8ca49
Wiltedmallard49 <69858723+Wiltedmallard49@users.noreply.github.com>
2020-09-10 10:22:59 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,40 @@
+'''
+dragon game
+creator: Adam 
+Description: flappy bird clone with dragon
+'''
+
+#setup
+info.set_life(3)
+info.set_score(0)
+#TODO add background
+#TODO add effects
+#create player
+Dragon = sprites.create(img("""
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+"""))
+Dragon.x = 35
+Dragon.ay = 150
+
+def on_flap():
+    Dragon.vy = -100
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_flap) 
+#create Chains
+
+#collision Detection
+
---
+++ main.ts
@@ -0,0 +1,35 @@
+/** 
+dragon game
+creator: Adam 
+Description: flappy bird clone with dragon
+
+ */
+// setup
+info.setLife(3)
+info.setScore(0)
+// TODO add background
+// TODO add effects
+// create player
+let Dragon = sprites.create(img`
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7
+`)
+Dragon.x = 35
+Dragon.ay = 150
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_flap() {
+    Dragon.vy = -100
+})
